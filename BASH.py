36c6cad966f3e9b86db8447e380135e18c31d54a
mikesharma253 <mikesharma253@gmail.com>
2026-09-11 19:58:37 +0530

    feat: add shooting, scoring, lives and game over

--- BASH.py
+++ BASH.py
@@ -205,19 +205,116 @@ if game_over == False:
 
 
 
-for bullet in bullets:
-    bullet[-1] -= bullets_speed
+        for bullet in bullets:
+            bullet[-1] -= bullets_speed
 
 
 #Remove bullet outside screen
 
-for bullet in bullets[:]:
+        for bullet in bullets[:]:
 
-    if bullet[1] <0:
-        bullets.remove(bullet)
+            if bullet[1] <0:
+            bullets.remove(bullet)
 
 
 #--------------------------------------------------------------
 #-----------------------now bullet+meteor----------------------
 #--------------------------------------------------------------
 
+
+        for bullet in bullets[:]:
+
+            bullet_rect = pygame.Rect(
+                bullet[0],
+                bullet[1],
+                5,
+                15
+            )
+
+            for meteor in meteors[:]:
+
+                meteor_rect = pygame.Rect(
+                            meteor[0],
+                            meteor[1],
+                            30,
+                            30
+                        )
+                        if check_collisition(bullet_rect, meteor_rect):
+
+                            if bullet in bullets
+                                bullet.remove(bullet)
+
+                            meteor[0] = random.randint(0, WIDTH - 30)
+                            meteor[1] = random.radint(-200, -30)
+
+                            score +=10
+
+                            break
+        #------------------------
+        # -----difficulty--------
+        # -----------------------
+
+
+        if score >= 50:
+            meteor_speed = 5
+
+        if score >= 100:
+            meteor_speed = 6
+
+        if score >= 200:
+            meteor_speed = 7
+
+
+    #---------------------------
+    # ---------draw-------------
+    # --------------------------
+
+
+    screen.fill(BLACK) 
+
+
+
+    if game_over == False:
+
+        #here is my boy
+        draw_player()
+
+        #meteors
+        for meteor in ranges:
+
+            pygame.draw.circle(
+                screen,
+                RED,
+                (meteor[0] + 15, meteor[1] + 15),
+                15
+            )
+
+        # BULLETs
+        for bullet in bullets:
+
+            pygame.draw.rect(
+                screen,
+                YELLOW,
+                (bullet[0], bullet[1], 5, 15)
+            )
+
+    #score
+    score_text = font.render(
+        "score: "+ str(score),
+        True,
+        WHITE
+    )
+
+    screen.blit(score_text,(20, 20))
+
+
+
+    #lives
+    lives_text = font.render(
+        "lives: "+ str(lives),
+        True,
+        GREEN
+    )
+
+    screen.blin(lives_text)
+     
